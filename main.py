1797bb70261e3241843e9a6109d064a3b17c2a59
starun96 <tarunsaharya@gmail.com>
2019-04-29 21:06:50 -0400

    added recursive function gen_tree to handle the circle tree generation

--- main.py
+++ main.py
@@ -3,9 +3,67 @@ from scipy import signal
 import random
 
 
-def gen_tree(center, heading, angle_span):
-    turtle.setpos(center)
-    turtle.setheading(heading)
+crush = turtle.Turtle()
+radius = 5
+line_length = 8
+
+
+def gen_tree(center, depth, heading=90, angle_span=360):
+    """ 
+    @param point where the circle will be drawn
+    @param depth how many times to recurse 
+    @param heading the general direction in which the turtle will form the 3 lines
+    @param angle_span how wide out the lines will span (in terms of angle)
+
+    recursively draw a circle and then 3 lines sticking out of it (which are the new centers for successive circles) until depth 0 
+    the angle span and heading angle specify the sector of the circle from which the lines will be jutting out (if the angle span is 120 and the heading is 0, then lines with headings 0, -60 (300), and 60 will be drawn from the center of the circle). 
+
+    first draw the circle around the [center], then go back to the center and set heading to [heading]. go forward by radius of circle (pen not on canvas). then go forward by the line length (pen on canvas). Record location as new center and the heading as the new heading. Go back to [center] and set heading to [heading - angle_span / 2]. repeat drawing the line. Go back to [center] and set heading to [heading + angle_span /2]. repeat drawing the line. 
+
+    Recursively call gen_tree on the 3 new points with 1/3 the original angle_span, the new center and heading, and depth - 1.
+     """
+    new_center_headings = []
+
+    # draw circle at [center]
+    crush.penup()
+    crush.setpos(center)
+    crush.forward(radius)
+    crush.left(90)
+    crush.pendown()
+    crush.circle(radius, 360)
+
+    # draw first line
+    crush.penup()
+    crush.setpos(center)
+    crush.setheading(heading)
+    crush.forward(radius)
+    crush.pendown()
+    crush.forward(line_length)
+    new_center_headings.append((crush.pos(), crush.heading()))
+
+    # draw second line
+    crush.penup()
+    crush.setpos(center)
+    crush.setheading(heading - angle_span / 2)
+    crush.forward(radius)
+    crush.pendown()
+    crush.forward(line_length)
+    new_center_headings.append((crush.pos(), crush.heading()))
+
+    # draw third line
+    crush.penup()
+    crush.setpos(center)
+    crush.setheading(heading + angle_span / 2)
+    crush.forward(radius)
+    crush.pendown()
+    crush.forward(line_length)
+    new_center_headings.append((crush.pos(), crush.heading()))
+
+    # make recursive call if depth > 0
+    if depth > 0:
+        for center_heading in new_center_headings:
+            gen_tree(center_heading[0], depth - 1,
+                     heading=center_heading[1], angle_span=angle_span / 3)
 
 
 def do_art(filepath):
@@ -13,30 +71,10 @@ def do_art(filepath):
     # initialize the turtle
     window = turtle.Screen()
     window.setup(width=800, height=600, startx=10, starty=0.5)
-    crush = turtle.Turtle()
     crush.shape("turtle")
 
-    """ current = 0
-    seen = set()
-
-    # step sizes increase by 1 each time
-    for step_size in range(1, 100):
-        backwards = current - step_size
-
-        if backwards > 0 and backwards not in seen:
-            crush.setheading(90)
-            crush.circle(step_size / 2, 180)
-            current = backwards
-
-            seen.add(current)
-
-        else:
-            crush.setheading(270)
-            crush.circle(step_size/2, 180)
-            seen.add(current) """
-
-    current_pos = crush.position()
-    crush.circle(30, 120)
+    # generate the circle tree
+    gen_tree((400, 300), 4)
 
     turtle.done()
 
